c99e554a7a35ef4fed7f432eb9cb1353b03d88fa
Pavel Tseva <paveltseva@gmail.com>
2026-02-21 13:42:01 +0200

    Move all code of extraction of user's permission from AuthorizationService to UserPermissionExtractor service

--- src/main/java/com/crm/auth/service/AuthorizationService.java
+++ src/main/java/com/crm/auth/service/AuthorizationService.java
@@ -1,16 +1,12 @@
 package com.crm.auth.service;
 
 import com.crm.auth.dto.JwtPayload;
-import com.crm.auth.feign.MainClient;
 import com.crm.auth.persistance.entity.redis.UserPermission;
 import com.crm.auth.service.checker.PermissionChecker;
-import com.crm.auth.service.operation.UserPermissionSaver;
+import com.crm.auth.service.operation.UserPermissionExtractor;
 import com.crm.auth.utils.JwtUtils;
 import com.crm.sharedlib.core.dto.request.AuthorizationRequest;
 import com.crm.sharedlib.core.dto.response.AuthResponse;
-import com.crm.sharedlib.core.dto.response.OrganizationUserRolesResponse;
-import com.crm.sharedlib.core.exception.ForbiddenException;
-import com.crm.sharedlib.core.exception.NotFoundException;
 import com.crm.sharedlib.core.exception.UnauthorizedException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -24,11 +20,8 @@ import static java.util.Objects.isNull;
 public class AuthorizationService {
 
     private final JwtService jwtService;
-    private final UserPermissionService userPermissionService;
-    private final UserPermissionSaver userPermissionSaver;
     private final PermissionChecker permissionChecker;
-
-    private final MainClient mainClient;
+    private final UserPermissionExtractor userPermissionExtractor;
 
     public AuthResponse authorize(String authorizationHeader) {
         JwtPayload payload = getToken(authorizationHeader);
@@ -43,8 +36,8 @@ public class AuthorizationService {
         JwtPayload payload = getToken(authorizationHeader, organizationId);
 
         UserPermission userPermission =
-                userPermissionService.getUserPermission(organizationId, (long) payload.getId()).
-                        orElseGet(() -> fetchUserPermissionFromMainServiceAndSave(organizationId, (long) payload.getId()));
+                // TODO: Fix casting int to long
+                userPermissionExtractor.getUserPermission(organizationId, (long) payload.getId());
 
         permissionChecker.checkUserPermission(userPermission, request);
 
@@ -64,25 +57,6 @@ public class AuthorizationService {
                 .orElseThrow(() -> new UnauthorizedException("Unauthorized"));
 
         return jwtService.getPayloadFromJwtToken(token);
-    }
-
-    private UserPermission fetchUserPermissionFromMainServiceAndSave(Long organizationId, Long userId) {
-        log.debug("User permission not found in cache: Organization = {}, User = {}",
-                organizationId, userId
-        );
-
-        OrganizationUserRolesResponse organizationUserRoles;
-
-        try {
-            organizationUserRoles =
-                    mainClient.getOrganizationUserRoles(organizationId, userId);
-        } catch (NotFoundException e) {
-            throw new ForbiddenException("Access controls not found");
-        }
-
-        return userPermissionSaver.saveUserPermission(
-                organizationId, userId, organizationUserRoles.getRolesId()
-        );
     }
 
 }
---
+++ src/main/java/com/crm/auth/service/operation/UserPermissionExtractor.java
@@ -0,0 +1,46 @@
+package com.crm.auth.service.operation;
+
+import com.crm.auth.feign.MainClient;
+import com.crm.auth.persistance.entity.redis.UserPermission;
+import com.crm.auth.service.UserPermissionService;
+import com.crm.sharedlib.core.dto.response.OrganizationUserRolesResponse;
+import com.crm.sharedlib.core.exception.ForbiddenException;
+import com.crm.sharedlib.core.exception.NotFoundException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class UserPermissionExtractor {
+
+    private final UserPermissionService userPermissionService;
+    private final UserPermissionSaver userPermissionSaver;
+
+    private final MainClient mainClient;
+
+    public UserPermission getUserPermission(Long organizationId, Long userId) {
+        return userPermissionService.getUserPermission(organizationId, userId).
+                orElseGet(() -> fetchUserPermissionFromMainServiceAndSave(organizationId, userId));
+    }
+
+    private UserPermission fetchUserPermissionFromMainServiceAndSave(Long organizationId, Long userId) {
+        log.debug("User permission not found in cache: Organization = {}, User = {}",
+                organizationId, userId
+        );
+
+        OrganizationUserRolesResponse organizationUserRoles;
+
+        try {
+            organizationUserRoles =
+                    mainClient.getOrganizationUserRoles(organizationId, userId);
+        } catch (NotFoundException e) {
+            throw new ForbiddenException("Access controls not found");
+        }
+
+        return userPermissionSaver.saveUserPermission(
+                organizationId, userId, organizationUserRoles.getRolesId()
+        );
+    }
+}
