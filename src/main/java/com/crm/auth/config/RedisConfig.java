22010e93e04c4a926cfe29370d83311c8980b2da
Pavel Tseva <paveltseva@gmail.com>
2026-03-08 11:30:35 +0200

    Changes value serializer for Redis

--- src/main/java/com/crm/auth/config/RedisConfig.java
+++ src/main/java/com/crm/auth/config/RedisConfig.java
@@ -1,21 +1,26 @@
 package com.crm.auth.config;
 
 import com.crm.sharedlib.rbac.dto.UserPermission;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializer;
 
 @Configuration
 public class RedisConfig {
 
     @Bean
-    RedisTemplate<String, UserPermission> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
-
+    RedisTemplate<String, UserPermission> redisTemplate(
+            RedisConnectionFactory redisConnectionFactory, ObjectMapper objectMapper
+    ) {
         RedisTemplate<String, UserPermission> template = new RedisTemplate<>();
         template.setConnectionFactory(redisConnectionFactory);
-        template.setValueSerializer(RedisSerializer.json());
+        GenericJackson2JsonRedisSerializer valueSerializer = new GenericJackson2JsonRedisSerializer(objectMapper);
+
+        template.setValueSerializer(valueSerializer);
         template.setKeySerializer(RedisSerializer.string());
         return template;
     }
